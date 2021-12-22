62364a14864c4c47e3c46cd19dd9b522dff55bd7
Laurens <laurens-morales@outlook.com>
2021-12-23 05:29:11 +1300

    im so confused

---
+++ script.js
@@ -0,0 +1,30 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  let title = prompt('Enter the title of the book');
+  let author = prompt('Enter the author');
+  let pages = prompt('Enter number of pages');
+  let read = false;
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+Book('Harry Potter', 'JK Rowling', '500', true);
+Book('The Hobbit', 'J.R.R. Tolkien', '349', false);
+
+function test() {
+  for (let key of myLibrary) {
+    let append = document.querySelector('div');
+    append.textContent += key.title;
+    append.textContent += key.author;
+    append.textContent += key.pages;
+    append.textContent += key.read;
+    append.textContent += '<br></br>';
+  }
+}

c0ddd1c1a4e38f13d9c667cef7421781007b530e
Laurens <laurens-morales@outlook.com>
2021-12-23 13:05:43 +1300

    Implement display books function

--- script.js
+++ script.js
@@ -22,16 +22,17 @@ myLibrary[1] = new Book('The Hobbit', 'J.R.R. Tolkien', '349', false);
 
 function displayBooks() {
   let newCard;
-  let body = document.querySelector('.body');
   for (let key in myLibrary) {
     newCard = new CreateElements(key);
-    if (myLibrary[key].read) {
+    if (myLibrary[key].read) { // checks if book has been read
       newCard.card.setAttribute('class', 'card complete');
       newCard.markCompleteBtn.textContent = 'Completed';
     }
     newCard.title.textContent = myLibrary[key].title;
     newCard.author.textContent = myLibrary[key].author;
-    newCard.pages.textContent = myLibrary[key].pages;
+    newCard.pages.textContent = `Pages: ${myLibrary[key].pages}`;
+    newCard.trashBtn.innerHTML = "<img src='./trash.svg'></img>"
+    appendCard(newCard);
   }
 }
 
@@ -41,18 +42,28 @@ function CreateElements(index) {
   this.card.setAttribute('class', 'card incomplete');
   this.textArea = document.createElement('div');
   this.textArea.setAttribute('class', 'text-area');
-  this.btnsArea = document.createElement('div');
-  this.btnsArea.setAttribute('class', 'button-area');
-  this.trashBtn = document.createElement('button');
-  this.trashBtn.setAttribute('class', 'trash');
-  this.markCompleteBtn = document.createElement('button');
-  this.markCompleteBtn.textContent = 'Mark as completed';
-
   this.title = document.createElement('h1');
   this.author = document.createElement('h3');
   this.pages = document.createElement('p');
+  this.btnsArea = document.createElement('div');
+  this.btnsArea.setAttribute('class', 'button-area');
+  this.markCompleteBtn = document.createElement('button');
+  this.markCompleteBtn.textContent = 'Mark as completed';
+  this.trashBtn = document.createElement('button');
+  this.trashBtn.setAttribute('class', 'trash');
 }
 
+function appendCard(newCard) {
+  let body = document.querySelector('.body');
+  body.prepend(newCard.card);
+  newCard.card.append(newCard.textArea);
+  newCard.textArea.append(newCard.title);
+  newCard.textArea.append(newCard.author);
+  newCard.textArea.append(newCard.pages);
+  newCard.card.append(newCard.btnsArea);
+  newCard.btnsArea.append(newCard.markCompleteBtn);
+  newCard.btnsArea.append(newCard.trashBtn);
+}
       // <div class='card incomplete'>
       //   <div class='text-area'>
       //     <h1>Placeholder Text</h1>
@@ -64,3 +75,6 @@ function CreateElements(index) {
       //     <button class='trash'><img src='./trash.svg'></img></button>
       //   </div>
       // </div>
+
+// -- initialize -- //
+displayBooks();
